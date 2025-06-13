f79e23465ccff0181c1499fb23f9c19deb7754d9
Dhiraj Rai <raibhuwan6018@gmail.com>
2025-06-13 15:38:03 +0545

    add 60.c

--- c100/06/60.c
+++ c100/06/60.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(void)
-{    
-    
+{
+    int num = 5;
+
+    int *ptr = &num;
+
+    printf("the value of num is %d\n", num);
+    *ptr = 50;
+    printf("the value of num is %d\n", num);
+
     return 0;
 }
